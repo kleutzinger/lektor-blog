e508add65dff63c930cc7bff6fca027bff72d5f5
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-09-25 02:46:58 -0700

    add small screenshots to homepage

---
+++ scripts/build-home.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+"""
+this builds the homepage for https://kevbot.xyz
+"""
+
+
+import os
+import requests
+from io import StringIO
+import csv
+
+# make dump generated file here
+# content dir is one directory up
+CONTENTS_LR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "content", "contents.lr")
+IMAGE_404 = "https://cdn.kevbot.xyz/file/kevbot-cdn/screenshots/404.png"
+CSV_DL_PATH = "https://docs.google.com/spreadsheets/d/1MVghM465zjbMGB89CwvH-GwfwyLXqHqfY1emM7Mp-uA/export?format=csv"
+TEMPLATE = """
+title: Kevin's Homepage
+---
+body:
+## [back to my visual portfolio here](https://kevinleutzinger.com)
+MARKDOWN
+---
+alt_note: alt note
+"""
+
+"""
+| Syntax | Description |
+| --- | ----------- |
+| Header | Title |
+| Paragraph | Text |
+"""
+
+
+def get_projects() -> list:
+    resp = requests.get(CSV_DL_PATH)
+    scsv = resp.text
+
+    f = StringIO(scsv)
+    reader = csv.reader(f, delimiter=",")
+    # convert csv to list of dicts
+    headers = next(reader)
+    rows = []
+    for row in reader:
+        row_dict = {}
+        for i, header in enumerate(headers):
+            if row[i]:
+                row_dict[header] = row[i]
+        rows.append(row_dict)
+    return rows
+
+
+def main():
+    global TEMPLATE
+    # get json from endpoint
+    projects = get_projects()
+    projects.sort(reverse=True, key=lambda x: x.get("date_created", "0"))
+    used_fields = ["date_created", "title/link", "source code", "screenshot_url"]
+    piped = "|".join(used_fields)
+    headers = f"|{piped}|"
+    spacer = f"|{'---|'*len(used_fields)}"
+    output_string = f"{headers}\n{spacer}\n"
+
+    def modify_project(project):
+        title = project.get("title", "title")
+        title_link = None
+        if "demo_url" in project:
+            title_link = project["demo_url"]
+        if "repo_url" in project:
+            project["source code"] = f"[source]({project['repo_url']})"
+            title_link = title_link or project["repo_url"]
+        if "readme_url" in project:
+            title_link = title_link or project["readme_url"]
+        if "star_rating" in project:
+            full_star = "★"
+            star_rating = project["star_rating"]
+            project["stars"] = full_star * int(float(star_rating))
+        title_link = title_link or "#"
+        project["title/link"] = f"[{title}]({title_link})"
+
+        return project
+
+    for project in map(modify_project, projects):
+        if "kb" in project.get("omit_from", []):
+            continue
+        piped = ""
+        for field in used_fields:
+            val = project.get(field, "")
+            if field == "screenshot_url":
+                if val:
+                    val = f'<a href="{val}"><img class=small_img src="{val}"></img></a>'
+                else:
+                    val = f'<img class=small_img src="{IMAGE_404}">'
+            piped += f"{val}|"
+        vals = f"|{piped}|\n"
+        output_string += vals
+
+    markdown_output = TEMPLATE.replace("MARKDOWN", output_string)
+    with open(CONTENTS_LR, "w") as f:
+        f.write(markdown_output)
+
+
+if __name__ == "__main__":
+    main()
